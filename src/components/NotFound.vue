0adb834fe644ddf729729518bca8e64ce30ad92f
lghou <yousseflghoumaigui@gmail.com>
2019-12-09 20:49:40 +0000

    Add 404 page

---
+++ src/components/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Not Found Page</h1>
+    <p>
+      Ooops! looks like you are loosing the way. Go back to
+      <router-link to="/">Home</router-link>
+    </p>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import store from "@/store.js";
 
 Vue.use(VueRouter);
 
@@ -25,9 +26,44 @@ const routes = [
         component: () =>
           import(
             /* webpackChunkName: "experienceDetails" */ "../views/ExperienceDetails"
-          )
+          ),
+        beforeEnter: (to, from, next) => {
+          let destination = store.destinations.find(
+            destination => destination.slug == to.params.slug
+          );
+          if (destination) {
+            let experience = destination.experiences.find(
+              destination => destination.slug === to.params.experienceSlug
+            );
+            if (experience) {
+              next();
+            } else {
+              next({ name: "NotFound" });
+            }
+          } else {
+            next({ name: "NotFound" });
+          }
+        }
       }
-    ]
+    ],
+    beforeEnter: (to, from, next) => {
+      let experience = store.destinations.find(
+        destination => destination.slug == to.params.slug
+      );
+      if (experience) {
+        next();
+      } else {
+        next({ name: "NotFound" });
+      }
+    }
+  },
+  {
+    path: "/404",
+    alias: "*",
+    name: "NotFound",
+    props: true,
+    component: () =>
+      import(/* webpackChunkName: "NotFound" */ "../components/NotFound")
   }
 ];
 
